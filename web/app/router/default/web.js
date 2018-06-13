346b98399d4040bd2d78b8b9b79c459317200583
rojer Chen <rojerchen@qq.com>
2018-06-13 21:36:04 +0800

    跳转api目录结构 & 简单的首页模板

--- web/app/router/default/web.js
+++ web/app/router/default/web.js
@@ -7,8 +7,6 @@ module.exports = app => {
   const { router, controller, middleware } = app;
   const subRouter = router.namespace('', middleware.front(), middleware.hook(app.hooks));
 
-  subRouter.get('home', '/', controller.webContent.index);
-
   subRouter.get('web-captcha', '/_/captcha', controller.webContent.captcha);
   subRouter.get('web-category', '/:key', controller.webContent.category);
   subRouter.get('web-detail', '/:key/:id', controller.webContent.detail);
